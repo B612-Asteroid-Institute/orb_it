feaae48364c0fb1a00a404bd1719a172b2ca4142
Aidan Berres <48331027+berres2002@users.noreply.github.com>
2022-06-30 14:14:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    packages=find_packages(
+      where='.',
+      include=['orb_it*']),
    use_scm_version={
       "write_to": "orb_it/version.py",
       "write_to_template": "__version__ = '{version}'",
